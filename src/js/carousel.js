80d91f2f920ba7829ea105f69efd6408c114efb0
techpedroso <technologypedroso@gmail.com>
2026-05-03 14:03:23 -0300

    Add carousel feat

---
+++ src/js/carousel.js
@@ -0,0 +1,49 @@
+function initCarousel() {
+    console.log("Hello, World!");
+    
+
+    const track = document.getElementById('track');
+    const items = document.querySelectorAll('.item');
+    const nextBtn = document.getElementById('nextBtn');
+    const prevBtn = document.getElementById('prevBtn');
+
+    let index = 0;
+    const gap = 20;
+
+    function updateCarousel() {
+        const itemWidth = items[0].offsetWidth;
+        const amountToMove = (itemWidth + gap) * index;
+        track.style.transform = `translateX(-${amountToMove}px)`;
+    }
+
+    function nextSlide() {
+        index = (index + 1) % items.length;
+        updateCarousel();
+        resetTimer();
+    }
+
+    function prevSlide() {
+        index = (index - 1 + items.length) % items.length;
+        updateCarousel();
+        resetTimer();
+    }
+
+    // nav btns
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+
+    // Autoplay
+    let autoPlay = setInterval(nextSlide, 5000);
+
+    function resetTimer() {
+        clearInterval(autoPlay);
+        autoPlay = setInterval(nextSlide, 3000);
+    }
+
+    // mouse hover = pause (it's lagging)
+    // const wrapper = document.getElementById('wrapper');
+    // wrapper.addEventListener('mouseenter', () => clearInterval(autoPlay));
+    // wrapper.addEventListener('mouseleave', () => autoPlay = setInterval(nextSlide, 5000));
+}
+
+export default initCarousel;
